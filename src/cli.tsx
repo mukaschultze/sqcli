d76be47416078ff2111dae75d79fad24b80abd73
Muka Schultze <samuelschultze@gmail.com>
2021-02-06 14:50:25 -0300

    project configuration

---
+++ src/cli.tsx
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import { render } from "ink";
+import meow from "meow";
+import React from "react";
+import App from "./app";
+
+const cli = meow(
+	`
+	Usage
+	  $ sqcli
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ sqcli --name=Jane
+	  Hello, Jane
+`,
+	{
+		flags: {
+			name: {
+				type: "string",
+			},
+		},
+	}
+);
+
+render(<App name={cli.flags.name} />);
---
+++ src/test.tsx
@@ -0,0 +1,17 @@
+import test from "ava";
+import chalk from "chalk";
+import { render } from "ink-testing-library";
+import React from "react";
+import App from "./app";
+
+test("greet unknown user", (t) => {
+	const { lastFrame } = render(<App />);
+
+	t.is(lastFrame(), chalk`Hello, {green Stranger}`);
+});
+
+test("greet user with a name", (t) => {
+	const { lastFrame } = render(<App name="Jane" />);
+
+	t.is(lastFrame(), chalk`Hello, {green Jane}`);
+});
