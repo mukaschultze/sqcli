13a9f4ee4014b60e36cd71da1cbbd71ac0af7586
Muka Schultze <samuelschultze@gmail.com>
2021-02-07 01:52:36 -0300

    add knex debug logs

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -9,6 +9,14 @@ export const Database = new Observable<Knex>((sub) => {
     connection: {
       filename: "./database.sqlite",
     },
+    debug: false,
+    log: {
+      debug: (message) => console.debug("debug", message),
+      deprecate: (message) => console.warn("deprecate", message),
+      error: (message) => console.error("error", message),
+      warn: (message) => console.warn("warn", message),
+      enableColors: true,
+    },
   });
 
   sub.next(pg);
